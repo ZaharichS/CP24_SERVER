461a77d29b793ccc4f5e74d1c81816384e7f6fc7
ZaharichS <zhlzny.ahkaz@gmail.com>
2024-05-07 15:48:27 +0300

    Initial

---
+++ src/main/java/ru/harmony/serverboot/ServerbootApplication.java
@@ -0,0 +1,13 @@
+package ru.harmony.serverboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/harmony/serverboot/ServerbootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.harmony.serverboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
